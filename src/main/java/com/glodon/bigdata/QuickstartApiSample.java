384084114fae49fe1e8f366920c316138b6af743
3B-4-12  郭流芳 <guolf@glodon.com>
2018-03-21 15:01:30 +0800

    获取到 Event 的数据

--- src/main/java/com/glodon/bigdata/QuickstartApiSample.java
+++ src/main/java/com/glodon/bigdata/QuickstartApiSample.java
@@ -20,6 +20,7 @@ import com.sforce.soap.enterprise.QueryResult;
 import com.sforce.soap.enterprise.SaveResult;
 import com.sforce.soap.enterprise.sobject.Account;
 import com.sforce.soap.enterprise.sobject.Contact;
+import com.sforce.soap.enterprise.sobject.Event;
 import com.sforce.soap.enterprise.sobject.SObject;
 import com.sforce.ws.ConnectorConfig;
 import com.sforce.ws.ConnectionException;
@@ -48,14 +49,15 @@ public class QuickstartApiSample {
         // Make a login call
         if (login()) {
             // Do a describe global
-            describeGlobalSample();
+//            看看这个表输出的是什么
+//            describeGlobalSample();
 
             // Describe an object
-            describeSObjectsSample();
-
+//            describeSObjectsSample();
+//搞清楚了，这是对每张表的详细解释。。。
             // Retrieve some data using a query
-            querySample();
-
+//            querySample();
+            querySampleTest();
             // Log out
             logout();
         }
@@ -300,6 +302,223 @@ public class QuickstartApiSample {
                             System.out.println("Contact " + (i + 1) + ": " + fName
                                     + " " + lName);
                         }
+                    }
+
+                    if (qr.isDone()) {
+                        done = true;
+                    } else {
+                        qr = connection.queryMore(qr.getQueryLocator());
+                    }
+                }
+            } else {
+                System.out.println("No records found.");
+            }
+        } catch (ConnectionException ce) {
+            ce.printStackTrace();
+        }
+    }
+
+    private void querySampleTest() {
+        String soqlQuery = "SELECT Id,WhoId,WhatId,WhoCount,WhatCount,Subject,Location,IsAllDayEvent,ActivityDateTime,ActivityDate,DurationInMinutes,StartDateTime,EndDateTime,Description,AccountId,OwnerId,CurrencyIsoCode,Type,IsPrivate,ShowAs,IsDeleted,IsChild,IsGroupEvent,GroupEventType,CreatedDate,CreatedById,LastModifiedDate,LastModifiedById,SystemModstamp,IsArchived,RecurrenceActivityId,IsRecurrence,RecurrenceStartDateTime,RecurrenceEndDateOnly,RecurrenceTimeZoneSidKey,RecurrenceType,RecurrenceInterval,RecurrenceDayOfWeekMask,RecurrenceDayOfMonth,RecurrenceInstance,RecurrenceMonthOfYear,ReminderDateTime,IsReminderSet,EventSubtype FROM Event";
+        try {
+            QueryResult qr = connection.query(soqlQuery);
+            boolean done = false;
+
+            if (qr.getSize() > 0) {
+                System.out.println("\nLogged-in user can see "
+                        + qr.getRecords().length + " Event records.");
+
+                while (!done) {
+                    System.out.println("");
+                    SObject[] records = qr.getRecords();
+                    for (int i = 0; i < records.length; ++i) {
+                        Event event = (Event) records[i];
+                        String id = "N/A";
+                        String whoid = "N/A";
+                        String whatid = "N/A";
+                        String whocount = "N/A";
+                        String whatcount = "N/A";
+                        String subject = "N/A";
+                        String location = "N/A";
+                        String isalldayevent = "N/A";
+                        String activitydatetime = "N/A";
+                        String activitydate = "N/A";
+                        String durationinminutes = "N/A";
+                        String startdatetime = "N/A";
+                        String enddatetime = "N/A";
+                        String description = "N/A";
+                        String accountid = "N/A";
+                        String ownerid = "N/A";
+                        String currencyisocode = "N/A";
+                        String type = "N/A";
+                        String isprivate = "N/A";
+                        String showas = "N/A";
+                        String isdeleted = "N/A";
+                        String ischild = "N/A";
+                        String isgroupevent = "N/A";
+                        String groupeventtype = "N/A";
+                        String createddate = "N/A";
+                        String createdbyid = "N/A";
+                        String lastmodifieddate = "N/A";
+                        String lastmodifiedbyid = "N/A";
+                        String systemmodstamp = "N/A";
+                        String isarchived = "N/A";
+                        String recurrenceactivityid = "N/A";
+                        String isrecurrence = "N/A";
+                        String recurrencestartdatetime = "N/A";
+                        String recurrenceenddateonly = "N/A";
+                        String recurrencetimezonesidkey = "N/A";
+                        String recurrencetype = "N/A";
+                        String recurrenceinterval = "N/A";
+                        String recurrencedayofweekmask = "N/A";
+                        String recurrencedayofmonth = "N/A";
+                        String recurrenceinstance = "N/A";
+                        String recurrencemonthofyear = "N/A";
+                        String reminderdatetime = "N/A";
+                        String isreminderset = "N/A";
+                        String eventsubtype = "N/A";
+                        if (event.getId() != null) {
+                            id = event.getId().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getWhoId() != null) {
+                            whoid = event.getWhoId().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getWhatId() != null) {
+                            whatid = event.getWhatId().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getWhoCount() != null) {
+                            whocount = event.getWhoCount().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getWhatCount() != null) {
+                            whatcount = event.getWhatCount().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getSubject() != null) {
+                            subject = event.getSubject().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getLocation() != null) {
+                            location = event.getLocation().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getIsAllDayEvent() != null) {
+                            isalldayevent = event.getIsAllDayEvent().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getActivityDateTime() != null) {
+                            activitydatetime = event.getActivityDateTime().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getActivityDate() != null) {
+                            activitydate = event.getActivityDate().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getDurationInMinutes() != null) {
+                            durationinminutes = event.getDurationInMinutes().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getStartDateTime() != null) {
+                            startdatetime = event.getStartDateTime().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getEndDateTime() != null) {
+                            enddatetime = event.getEndDateTime().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getDescription() != null) {
+                            description = event.getDescription().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getAccountId() != null) {
+                            accountid = event.getAccountId().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getOwnerId() != null) {
+                            ownerid = event.getOwnerId().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getCurrencyIsoCode() != null) {
+                            currencyisocode = event.getCurrencyIsoCode().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getType() != null) {
+                            type = event.getType().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getIsPrivate() != null) {
+                            isprivate = event.getIsPrivate().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getShowAs() != null) {
+                            showas = event.getShowAs().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getIsDeleted() != null) {
+                            isdeleted = event.getIsDeleted().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getIsChild() != null) {
+                            ischild = event.getIsChild().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getIsGroupEvent() != null) {
+                            isgroupevent = event.getIsGroupEvent().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getGroupEventType() != null) {
+                            groupeventtype = event.getGroupEventType().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getCreatedDate() != null) {
+                            createddate = event.getCreatedDate().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getCreatedById() != null) {
+                            createdbyid = event.getCreatedById().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getLastModifiedDate() != null) {
+                            lastmodifieddate = event.getLastModifiedDate().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getLastModifiedById() != null) {
+                            lastmodifiedbyid = event.getLastModifiedById().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getSystemModstamp() != null) {
+                            systemmodstamp = event.getSystemModstamp().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getIsArchived() != null) {
+                            isarchived = event.getIsArchived().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getRecurrenceActivityId() != null) {
+                            recurrenceactivityid = event.getRecurrenceActivityId().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getIsRecurrence() != null) {
+                            isrecurrence = event.getIsRecurrence().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getRecurrenceStartDateTime() != null) {
+                            recurrencestartdatetime = event.getRecurrenceStartDateTime().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getRecurrenceEndDateOnly() != null) {
+                            recurrenceenddateonly = event.getRecurrenceEndDateOnly().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getRecurrenceTimeZoneSidKey() != null) {
+                            recurrencetimezonesidkey = event.getRecurrenceTimeZoneSidKey().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getRecurrenceType() != null) {
+                            recurrencetype = event.getRecurrenceType().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getRecurrenceInterval() != null) {
+                            recurrenceinterval = event.getRecurrenceInterval().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getRecurrenceDayOfWeekMask() != null) {
+                            recurrencedayofweekmask = event.getRecurrenceDayOfWeekMask().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getRecurrenceDayOfMonth() != null) {
+                            recurrencedayofmonth = event.getRecurrenceDayOfMonth().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getRecurrenceInstance() != null) {
+                            recurrenceinstance = event.getRecurrenceInstance().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getRecurrenceMonthOfYear() != null) {
+                            recurrencemonthofyear = event.getRecurrenceMonthOfYear().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getReminderDateTime() != null) {
+                            reminderdatetime = event.getReminderDateTime().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getIsReminderSet() != null) {
+                            isreminderset = event.getIsReminderSet().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+                        if (event.getEventSubtype() != null) {
+                            eventsubtype = event.getEventSubtype().toString().replaceAll("[\r\n]+", "&&").replaceAll("`", "");
+                        }
+
+                        System.out.println(id + "`" + whoid + "`" + whatid + "`" + whocount + "`" + whatcount + "`" + subject + "`" + location + "`" + isalldayevent + "`" + activitydatetime + "`" + activitydate + "`" + durationinminutes + "`" + startdatetime + "`" + enddatetime + "`" + description + "`" + accountid + "`" + ownerid + "`" + currencyisocode + "`" + type + "`" + isprivate + "`" + showas + "`" + isdeleted + "`" + ischild + "`" + isgroupevent + "`" + groupeventtype + "`" + createddate + "`" + createdbyid + "`" + lastmodifieddate + "`" + lastmodifiedbyid + "`" + systemmodstamp + "`" + isarchived + "`" + recurrenceactivityid + "`" + isrecurrence + "`" + recurrencestartdatetime + "`" + recurrenceenddateonly + "`" + recurrencetimezonesidkey + "`" + recurrencetype + "`" + recurrenceinterval + "`" + recurrencedayofweekmask + "`" + recurrencedayofmonth + "`" + recurrenceinstance + "`" + recurrencemonthofyear + "`" + reminderdatetime + "`" + isreminderset + "`" + eventsubtype);
+//                        if (fName == null) {
+//                            System.out.println("Contact " + (i + 1) + ": " + lName);
+//                        } else {
+//                            System.out.println("Contact " + (i + 1) + ": " + fName
+//                                    + " " + lName);
+//                        }
+
+
                     }
 
                     if (qr.isDone()) {
